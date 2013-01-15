9847b63fcd5a7705ccba52100065d9f9d06a7567
Jose Narvaez <goyox86@gmail.com>
2013-01-15 12:48:40 -0300

    Inital commit

---
+++ lib/yell/splunk.rb
@@ -0,0 +1,5 @@
+module Yell; end
+
+class Yell::Splunk
+  VERSION = "1.0.0"
+end
---
+++ test/yell/test_splunk.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require "yell/splunk"
+
+module TestYell; end
+
+class TestYell::TestSplunk < Minitest::Unit::TestCase
+  def test_sanity
+    flunk "write tests or I will kneecap you"
+  end
+end
